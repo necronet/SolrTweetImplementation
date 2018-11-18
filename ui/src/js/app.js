93ddf9b4f69ad1fa1308dcdf8386c30ff61810d3
Jose Ayerdis <joseayerdis@gmail.com>
2018-11-17 21:04:38 -0500

    Add handler to the app

--- ui/src/js/app.js
+++ ui/src/js/app.js
@@ -3,10 +3,19 @@ import { render } from 'react-dom';
 import style from "./app.less";
 
 export default class App extends Component {
+
+  handleKeyPress(e) {
+    if (e.key === 'Enter') {
+      console.log("Searching")
+    }
+
+  }
+
   render() {
     return (
       <div className={style.header}>
         Application boilerplate for Solr search
+        <input type="text" name="search" onKeyPress={this.handleKeyPress} />
       </div>
     );
   }
