cd3f95ba73d816f765dce0441170cf1eb42915d4
Jose Ayerdis <joseayerdis@gmail.com>
2018-11-26 04:48:18 -0500

    Very naive way to put the tweet in place

--- ui/src/js/app.js
+++ ui/src/js/app.js
@@ -1,24 +1,38 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import style from "./app.less";
+import style from "./App.less";
 import axios from 'axios';
+import ResultList from './ResultList'
 
 // Constants
-const SOLR_SERVER="http://localhost:8888/search/solr/tweets/select?q=mexico"
+const SOLR_SERVER="http://localhost:8888/search/solr/tweets/select?q=mexico&wt=json"
 var config = {
     headers: {'crossDomain': true}
 };
 
-
 export default class App extends Component {
+
+  constructor() {
+    super()
+    this.state = { results:[] }
+  }
+
+  componentWillMount() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    axios.get(SOLR_SERVER,config)
+    .then(res => {
+      //console.log(res.data.response);
+      this.setState({results:res.data.response.docs});
+    })
+  }
 
   handleKeyPress(e) {
     if (e.key === 'Enter') {
       console.log("Searching on " + SOLR_SERVER);
-      axios.get(SOLR_SERVER,config)
-      .then(res => {
-        console.log(res);
-      })
+      fetchData();
     }
 
   }
@@ -28,9 +42,10 @@ export default class App extends Component {
       <div className={style.header}>
         Application boilerplate for Solr search
         <input type="text" name="search" onKeyPress={this.handleKeyPress} />
+        <ResultList results={this.state.results} />
       </div>
     );
   }
 }
 
-render(<App />, document.getElementById('app'));
+render(<App />, document.getElementById('App'));
